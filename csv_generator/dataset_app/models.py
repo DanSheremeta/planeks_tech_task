c3ac7c2de5898e7f1bb0bec1362640fd2073f5c7
DanSheremeta <danylo3002@gmail.com>
2023-01-30 00:56:58 +0200

    created models

--- csv_generator/dataset_app/models.py
+++ csv_generator/dataset_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+SCHEMA_TYPE_CHOICE = (
+    ('Full Name', 'Full Name'),
+    ('Job', 'Job'),
+    ('Email', 'Email'),
+    ('Company', 'Company'),
+    ('Phone Number', 'Phone Number'),
+    ('Address', 'Address'),
+    ('Date', 'Date'),
+)
+
+
+class Column(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=SCHEMA_TYPE_CHOICE, default='Full Name')
+    order = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class Schema(models.Model):
+    title = models.CharField(max_length=100)
+    columns = models.ManyToManyField(Column, related_name='schema_columns')
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
