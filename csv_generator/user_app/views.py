fc93b1e4b9839bcc3ed36bc310fffbc34bf30c6d
DanSheremeta <danylo3002@gmail.com>
2023-01-30 00:58:41 +0200

    implemented login and logout system

--- csv_generator/user_app/views.py
+++ csv_generator/user_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('schemas-list')
+        else:
+            messages.success(request, 'Wrong username or password')
+            return redirect('login')
+    else:
+        return render(request, 'authentication/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You were successfully logged out')
+    return redirect('login')
