95c77c3e83b6f2923e51f0401358ee487ee392da
DanSheremeta <danylo3002@gmail.com>
2023-01-31 19:16:45 +0200

    created models

--- csv_generator/dataset_app/models.py
+++ csv_generator/dataset_app/models.py
@@ -11,20 +11,20 @@ SCHEMA_TYPE_CHOICE = (
 )
 
 
-class Column(models.Model):
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length=100, choices=SCHEMA_TYPE_CHOICE, default='Full Name')
-    order = models.PositiveIntegerField(default=1)
-
-    def __str__(self):
-        return self.name
-
-
 class Schema(models.Model):
-    title = models.CharField(max_length=100)
-    columns = models.ManyToManyField(Column, related_name='schema_columns')
+    title = models.CharField(max_length=100, unique=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Column(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=SCHEMA_TYPE_CHOICE, default='Full Name')
+    order = models.PositiveIntegerField(default=1)
+    schema = models.ForeignKey(Schema, on_delete=models.CASCADE, related_name='column_schema')
+
+    def __str__(self):
+        return self.name
