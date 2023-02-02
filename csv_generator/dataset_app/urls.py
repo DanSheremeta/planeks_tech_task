2f118010a0ad3a808a12bc2879f5b76fb13ab92e
DanSheremeta <danylo3002@gmail.com>
2023-02-03 01:18:00 +0200

    implemented urls

--- csv_generator/dataset_app/urls.py
+++ csv_generator/dataset_app/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
-    path('schemas/', views.schemas_list, name='schemas-list'),
+    path('schemas/', views.SchemaListView.as_view(), name='schemas-list'),
     path('schemas/create/', views.schema_create, name='schema-create'),
+    path('schemas/update/<int:pk>/', views.schema_update, name='schema-update'),
     path('schemas/delete/<int:pk>/', views.schema_delete, name='schema-delete'),
 
-    path('schemas/<int:pk>/create-column/', views.schema_column_create, name='column-create'),
     path('delete-column/<int:pk>/', views.column_delete, name='column-delete'),
 
-    path('schema/<int:pk>/csv-generate/', views.dataset_list, name='dataset-list'),
+    path('schema/<int:pk>/datasets/', views.dataset_list, name='dataset-list'),
+    path('schema/<int:pk>/csv-generate/', views.generate_csv, name='csv-generate'),
+    path('schema/<int:pk>/csv-download/', views.download_csv, name='csv-download'),
 ]
